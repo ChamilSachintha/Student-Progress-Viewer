a736b1e14eda88bf45635df64c8747c44838e19e
Chamil Sachintha <87272440+ChamilSachintha@users.noreply.github.com>
2022-11-23 14:35:54 +0530

    NavBar updated

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -40,20 +40,20 @@ export const NavBar = () => {
                 </Navbar.Toggle>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
-                        <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
-                        <Nav.Link href="#project" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
-                        <Nav.Link href="#feedbacks" className={activeLink === 'feedbacks' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('feedbacks')}>Feedback</Nav.Link>
+                        <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>HOME</Nav.Link>
+                        <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>GPA</Nav.Link>
+                        <Nav.Link href="#project" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>RESULTS</Nav.Link>
+                        <Nav.Link href="#feedbacks" className={activeLink === 'feedbacks' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('feedbacks')}>ATTENDANCE</Nav.Link>
                     </Nav>
                     <span className="navbar-text">
-                        <div className="social-icon">
+                        {/* <div className="social-icon">
                             <a href="https://www.linkedin.com/in/chamilsachintha96" target="_blank"><img src={navIcon1} alt="" /></a>
                             <a href="https://github.com/ChamilSachintha" target="_blank"><img src={navIcon4} alt="" /></a>
                             <a href="https://www.facebook.com/chamil.sachintha" target="_blank"><img src={navIcon2} alt="" /></a>
                             <a href="https://www.instagram.com/chamil_sachintha/" target="_blank"><img src={navIcon3} alt="" /></a>
-                        </div>
+                        </div> */}
                         <a href="https://drive.google.com/file/d/1p8dqvrfGXud8TBt_ZcEt3CkgTJdo9KGH/view?usp=sharing" target="_blank">
-                        <button className="vvd" onClick={() => console.log('connect')}><span>View CV</span></button>
+                        <button className="vvd" onClick={() => console.log('connect')}><span>WELCOME 2018/E/xxx</span></button>
                         </a>
                     </span>
                 </Navbar.Collapse>
