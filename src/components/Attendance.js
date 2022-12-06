d08b0de5b7d4b81a831cda8ca2312204e3bcda27
Chamil Sachintha <87272440+ChamilSachintha@users.noreply.github.com>
2022-12-06 10:22:29 +0530

    Circular progress bar added

--- src/components/Attendance.js
+++ src/components/Attendance.js
@@ -12,80 +12,124 @@ import meter8 from "../assets/img/meter8.png";
 import colorSharp from "../assets/img/color-sharp.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
+import {
+    CircularProgressbar,
+    CircularProgressbarWithChildren,
+    buildStyles
+} from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+
+const percentage = 66;
 
 export const Attendance = () => {
     const responsive = {
         superLargeDesktop: {
-          // the naming can be any, depends on you.
-          breakpoint: { max: 4000, min: 3000 },
-          items: 5
+            // the naming can be any, depends on you.
+            breakpoint: { max: 4000, min: 3000 },
+            items: 5
         },
         desktop: {
-          breakpoint: { max: 3000, min: 1024 },
-          items: 3
+            breakpoint: { max: 3000, min: 1024 },
+            items: 3
         },
         tablet: {
-          breakpoint: { max: 1024, min: 464 },
-          items: 2
+            breakpoint: { max: 1024, min: 464 },
+            items: 2
         },
         mobile: {
-          breakpoint: { max: 464, min: 0 },
-          items: 1
+            breakpoint: { max: 464, min: 0 },
+            items: 1
         }
-      };
+    };
 
-      return (
+    return (
         <section className="skill" id="skills">
             <Container>
                 <Row>
                     <Col>
-                    <div className="skill-bx">
-                    <TrackVisibility>
-                        {({ isVisible }) => 
-                            <div className={isVisible ? "animate__animated animate__slideInUp" : ""}>
-                        <h2>ATTENDANCE</h2>
-                        </div> }
-                        </TrackVisibility>
-                        <p>“Success usually comes to those who are too busy looking for it.”</p>
-                        <Carousel responsive={responsive} infinite={true} className="skill-slider">
-                            <div className="item">
-                                <img src={meter2} alt="Image" />
-                                <h5>7th Sem GPA</h5>
-                            </div>
-                            <div className="item-main">
-                                <img src={meter8} alt="Image" />
-                                <h5>Overall GPA</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter3} alt="Image" />
-                                <h5>1st Sem GPA</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter4} alt="Image" />
-                                <h5>2nd Sem GPA</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter5} alt="Image" />
-                                <h5>3rd Sem GPA</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter6} alt="Image" />
-                                <h5>4th Sem GPA</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter7} alt="Image" />
-                                <h5>5th Sem GPA</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>6th Sem GPA</h5>
-                            </div>
-                        </Carousel>
-                    </div>
+                        <div className="skill-bx">
+                            <TrackVisibility>
+                                {({ isVisible }) =>
+                                    <div className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                                        <h2>ATTENDANCE</h2>
+                                    </div>}
+                            </TrackVisibility>
+                            <p>“Success usually comes to those who are too busy looking for it.”</p>
+                            <Carousel responsive={responsive} infinite={true} className="skill-slider">
+                                <div className="item progress-container">
+                                    <CircularProgressbar
+                                        value={percentage}
+                                        strokeWidth={18}
+                                        text={`3.7`}
+                                        styles={buildStyles({
+                                            textColor: "#B8B8B8",
+                                            pathColor: "#7500e3",
+                                            trailColor: "#404040"
+                                        })}
+                                    />
+                                    <h5>7th Sem GPA</h5>
+                                </div> 
+                                <div className="item-main progress-container">
+                                    <CircularProgressbar
+                                        value={percentage}
+                                        strokeWidth={18}
+                                        text={`3.7`}
+                                        styles={buildStyles({
+                                            textColor: "#B8B8B8",
+                                            pathColor: "#7500e3",
+                                            trailColor: "#404040"
+                                        })}
+                                    />
+                                    <h5>Overall GPA</h5>
+                                </div>
+                                <div className="item progress-container">
+                                <CircularProgressbar
+                                        value={percentage}
+                                        strokeWidth={18}
+                                        text={`3.7`}
+                                        styles={buildStyles({
+                                            textColor: "#B8B8B8",
+                                            pathColor: "#7500e3",
+                                            trailColor: "#404040"
+                                        })}
+                                    />
+                                    <h5>1st Sem GPA</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={meter4} alt="Image" />
+                                    <h5>2nd Sem GPA</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={meter5} alt="Image" />
+                                    <h5>3rd Sem GPA</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={meter6} alt="Image" />
+                                    <h5>4th Sem GPA</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={meter7} alt="Image" />
+                                    <h5>5th Sem GPA</h5>
+                                </div>
+                                <div className="item progress-container">
+                                <CircularProgressbar
+                                        value={percentage}
+                                        strokeWidth={18}
+                                        text={`3.7`}
+                                        styles={buildStyles({
+                                            textColor: "#B8B8B8",
+                                            pathColor: "#7500e3",
+                                            trailColor: "#404040"
+                                        })}
+                                    />
+                                    <h5>6th Sem GPA</h5>
+                                </div>
+                            </Carousel>
+                        </div>
                     </Col>
                 </Row>
             </Container>
             <img className="background-image-left" src={colorSharp} alt="Image" />
         </section>
-      )
+    )
 }
