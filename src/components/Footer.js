40894227391795100b589c2cbfb4d5343555f5f5
Chamil Sachintha <87272440+ChamilSachintha@users.noreply.github.com>
2022-12-21 05:26:20 +0530

    Footer size changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,12 +16,12 @@ export const Footer = () => {
                         <img src={logo} alt="Logo" />
                     </Col> */}
                     <Col sm={7} className="text-center text-sm-end">
-                        <div className="footer-social-icon">
+                        {/* <div className="footer-social-icon">
                             <a href="https://www.linkedin.com/in/chamilsachintha96" target="_blank"><img src={navIcon1} alt="" /></a>
                             <a href="https://github.com/ChamilSachintha" target="_blank"><img src={navIcon4} alt="" /></a>
                             <a href="https://www.facebook.com/chamil.sachintha" target="_blank"><img src={navIcon2} alt="" /></a>
                             <a href="https://www.instagram.com/chamil_sachintha/" target="_blank"><img src={navIcon3} alt="" /></a>
-                        </div>
+                        </div> */}
                         <p>Copyright 2022. All Rights Reserved.</p>
                     </Col>  
                 </Row>
