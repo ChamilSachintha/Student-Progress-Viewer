0661da8ecae20d26b9341be6ebdaa3711da150fd
Chamil Sachintha <87272440+ChamilSachintha@users.noreply.github.com>
2023-09-01 20:43:04 +0530

    Remove user authentication to give everyone to login access

--- src/App.js
+++ src/App.js
@@ -10,106 +10,60 @@ import React, { useState } from "react";
 import { AlignCenter } from 'react-bootstrap-icons';
 import banner from "./assets/img/banner-login.png";
 
-
 function App() {
-
-  const [errorMessages, setErrorMessages] = useState({});
   const [isSubmitted, setIsSubmitted] = useState(false);
 
-  // User Login info
-  const database = [
-    {
-      username: "2018e093",
-      password: "123"
-    },
-    {
-      username: "2018e104",
-      password: "abc"
-    },
-    {
-      username: "abc",
-      password: "123"
-    }
-  ];
-
-  const errors = {
-    uname: "invalid username",
-    pass: "invalid password"
-  };
-
   const handleSubmit = (event) => {
-    //Prevent page reload
+    // Prevent page reload
     event.preventDefault();
-
-    var { uname, pass } = document.forms[0];
-
-    // Find user login info
-    const userData = database.find((user) => user.username === uname.value);
-
-    // Compare user info
-    if (userData) {
-      if (userData.password !== pass.value) {
-        // Invalid password
-        setErrorMessages({ name: "pass", message: errors.pass });
-      } else {
-        setIsSubmitted(true);
-      }
-    } else {
-      // Username not found
-      setErrorMessages({ name: "uname", message: errors.uname });
-    }
+    setIsSubmitted(true);
   };
-
-  // Generate JSX code for error message
-  const renderErrorMessage = (name) =>
-    name === errorMessages.name && (
-      <div className="error1">{errorMessages.message}</div>
-    );
 
   // JSX code for login form
   const renderForm = (
     <div className="form1" id='form1'>
-      
       <form onSubmit={handleSubmit}>
-      <div>
-        <h3>Student Progress Viewer</h3>
-        <br></br>
-      </div>
-        <div className="input-container1">
+        <div>
+          <h3>Student Progress Viewer</h3>
+          <br></br>
+          
+<div className="input-container1">
           <label>Username </label>
-          <input type="text" name="uname" required />
-          {renderErrorMessage("uname")}
+          <input type="text" name="uname" />
+          {/* {renderErrorMessage("uname")} */}
         </div>
         <div className="input-container1">
           <label>Password </label>
-          <input type="password" name="pass" required />
-          {renderErrorMessage("pass")}
+          <input type="password" name="pass" />
+          {/* {renderErrorMessage("pass")} */}
+        </div>
         </div>
         <div className="button-container1">
           <input type="submit" />
         </div>
       </form>
       <img className="background-image" src={banner} alt="Image" />
-
     </div>
   );
 
   return (
     <div className="app">
       <div className="login-form">
-        {isSubmitted ? <div>
-          <NavBar />
-          <Banner />
-          <GPA />
-          <Results />
-          <Attendance />
-          <Footer />
-        </div> : renderForm}
+        {isSubmitted ? (
+          <div>
+            <NavBar />
+            <Banner />
+            <GPA />
+            <Results />
+            <Attendance />
+            <Footer />
+          </div>
+        ) : (
+          renderForm
+        )}
       </div>
     </div>
   );
-
- 
 }
 
 export default App;
