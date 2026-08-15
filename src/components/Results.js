4924933232e5b9838370d63159784609751fe69d
Chamil Sachintha <87272440+ChamilSachintha@users.noreply.github.com>
2026-08-15 14:19:31 +0530

    fix: resolve image name collision for banner-bg1 background and decoration

--- src/components/Results.js
+++ src/components/Results.js
@@ -5,7 +5,7 @@ import { ProjectCard3 } from "./ProjectCards/ProjectCard3";
 import { ProjectCard4 } from "./ProjectCards/ProjectCard4";
 import { ProjectCard5 } from "./ProjectCards/ProjectCard5";
 import TrackVisibility from 'react-on-screen';
-import colorSharp2 from "../assets/img/banner-bg1.webp";
+import colorSharp2 from "../assets/img/banner-bg1-decor.webp";
 
 import meter7 from "../assets/img/meter7.webp";
 
